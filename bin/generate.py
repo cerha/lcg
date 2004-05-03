32eb31a7652730eccc25c50a80bc2c72d634e62e
cerha <cerha>
2004-05-03 19:22:52 +0000

    Use `EurochanceCourse' instead of `Course'. Don't use the `lang' argument.

--- bin/generate.py
+++ bin/generate.py
@@ -3,8 +3,7 @@
 import lcg
 import sys
 
-lang = 'en'
 source_dir = sys.argv[1]
 destination_dir = sys.argv[2]
 
-lcg.course.Course(source_dir, lang).export(destination_dir)
+lcg.course.EurochanceCourse(source_dir).export(destination_dir)
