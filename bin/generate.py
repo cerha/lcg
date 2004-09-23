a22ddd745202297c9b7efa10bb452b36aec89d41
cerha <cerha>
2004-09-23 20:15:20 +0000

    (course): Take the stylesheet name as a third argument and pass it to the StaticExporter constructor.

--- bin/generate.py
+++ bin/generate.py
@@ -5,8 +5,11 @@ import sys
 
 source_dir = sys.argv[1]
 destination_dir = sys.argv[2]
+stylesheet = len(sys.argv) > 3 and sys.argv[3] or None
 
-course = lcg.course.EurochanceCourse(source_dir)
+c = lcg.course.EurochanceCourse(source_dir)
 
-#lcg.ims.IMSExporter(course, destination_dir).export()
-lcg.export.StaticExporter(course, destination_dir).export()
+#e = lcg.ims.IMSExporter(c, destination_dir)
+e = lcg.export.StaticExporter(c, destination_dir, stylesheet=stylesheet)
+
+e.export()
