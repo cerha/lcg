dbb4fdc9f286ef91a43778a16edbde6ed76d583c
cerha <cerha>
2005-03-02 16:36:52 +0000

    New argument `LCG dir'.  Use it to locate translations and default resources.

--- bin/generate.py
+++ bin/generate.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import gettext
 
 try:
-    source_dir, destination_dir, lang = sys.argv[1:4]
-    stylesheet = len(sys.argv) > 4 and sys.argv[4] or None
+    source_dir, destination_dir, lcg_dir, lang = sys.argv[1:5]
+    stylesheet = len(sys.argv) > 5 and sys.argv[5] or None
 except ValueError:
-    raise Exception("Usage: %s source_dir destination_dir lang [stylesheet]" % \
-                    sys.argv[0])
+    msg = "Usage: %s source_dir destination_dir lcg_dir lang [stylesheet]"
+    raise Exception(msg % sys.argv[0])
 
-t = gettext.translation('lcg', 'translations', (lang,), fallback=True)
+resource_dir = os.path.join(lcg_dir, 'resources')
+translation_dir = os.path.join(lcg_dir, 'translations')
+
+t = gettext.translation('lcg', translation_dir, (lang,), fallback=True)
 t.install(unicode=True)
 
 import lcg
-c = lcg.course.EurochanceCourse(source_dir, language=lang,
-                                input_encoding='utf-8')
+c = lcg.EurochanceCourse(source_dir, language=lang, input_encoding='utf-8',
+                         default_resource_dir=resource_dir)
 #e = lcg.ims.IMSExporter(c, destination_dir)
-e = lcg.export.StaticExporter(c, destination_dir, stylesheet=stylesheet)
-
+e = lcg.StaticExporter(c, destination_dir, stylesheet=stylesheet)
 e.export()
