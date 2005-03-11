6f3f9d79e19538e08a53be66006258e69507e646
cerha <cerha>
2005-03-11 11:55:42 +0000

    (Formatter._MARKUP): Uncomment the rules for `italic' and `fixed'.  The pair markup defined now as one item. New rules for `link', `uri', `email', `comment' and `rule' (Formatter._HELPER_PATTERNS): New constant. (Formatter._FORMAT): New formats for `comment' and `rule'. (Formatter._REGEXPS, Formatter._RULES): Constants removed. (Formatter.__init__): Compile the rules here. (Formatter._markup_handler): Check whether the type is not in `_HELPER_PATTERNS'. (Formatter._markup_handler): Pass the match groupdict to the formatter. (Formatter._formatter): New argument `groups'.  Pass it to the spicialized formatter. (Formatter._link_formatter, Formatter._uri_formatter) (Formatter._email_formatter): New methods. (Formatter.format): Use `self._rules', not `self._RULES'.  Pass an empty groupdict to the closing formatter. (Parser._SECTION_RE): Allow the anchor name behind the heading. (Parser._PRE_BLOCK_RE): New regexp. (Parser._MATCHERS): New rule for `toc'. (Parser._HELPER_PATTERNS): New `title' pattern. (Parser._Section.__init__): New argument `anchor'. (Parser._parse_sections): Honour it. (Parser._parse_section_content): Search for the preformatter blocks here. (Parser._parse_blocks): New method.  Everything whar was before in `_parse_section_content()' was copied here. (Parser._make_toc): New method.

--- lib/lcg/wiki.py
+++ lib/lcg/wiki.py
@@ -28,7 +28,7 @@ class Formatter(object):
     """Simple Wiki markup formatter.
 
     This simple Wiki formatter can only format the markup within one block (ie.
-    a single paragraph or other non-structured piecece of text).
+    a single paragraph or other non-structured piece of text).
 
     All the document structure (headings, paragraphs, bullet lists etc.) should
     be recognized by the 'Parser' class below.  Then only the contents of the
@@ -37,37 +37,51 @@ class Formatter(object):
 
     """
     _MARKUP = (('bold', '\*'),
-               #('italic', '/'),
-               #('fixed', '='),
+               ('italic', '/'),
+               ('fixed', '='),
                ('underline', '_'),
-               ('citation_start', '>>'),
-               ('citation_end', '<<'),
-               ('quotation_start', '``'),
-               ('quotation_end', "''"),
+               ('citation', ('>>', '<<')),
+               ('quotation', ('``', "''")),
+               ('link', '\[(?P<href>[^\]\|]+)(?:\|(?P<title>[^\]]+))?\]'),
+               ('uri', '(https?|ftp)://\S+?(?=[\),.:;]?(\s|$))'),
+               ('email', '\w[\w\-\.]*@\w[\w\-\.]+'),
+               ('comment', '^#.*'),
                ('linebreak', '//'),
+               ('rule', '^----\s*$'),
                ('nbsp', '~'),
                )
+    _HELPER_PATTERNS = ('href', 'title')
     
+    # The list below lists the which elements are paired on the output
+    # (formatter) side, not the input (markup) side (they must be opened and
+    # then closed individually).
     _PAIR = ('bold', 'italic', 'fixed', 'underline', 'citation', 'quotation')
-
+    
     _FORMAT = {'bold': ('<strong>', '</strong>'),
                'italic': ('<i>', '</i>'),
                'fixed': ('<tt>', '</tt>'),
                'underline': ('<span class="underline">', '</span>'),
                'quotation': (u'“<span class="quotation">', u'</span>”'),
-               'citation': ('<span class="citation">**', '**</span>'),
-               'linebreak': '<br>',
+               'citation': ('<span class="citation">', '</span>'),
+               'comment': '',
+               'linebreak': '<br/>',
+               'rule': '<hr/>',
                'nbsp': '&nbsp;',
                }
 
-    _REGEXPS = [r"(?P<%s>\!?%s)" % (tag, markup) for tag, markup in _MARKUP]
-    _RULES = re.compile('(?:' + '|'.join(_REGEXPS) + ')')
-
     def __init__(self, parent):
+        regexp = "(?P<%s>\!?%s)"
+        regexps = [isinstance(markup, types.StringType)
+                   and regexp % (type, markup)
+                   or '|'.join((regexp, regexp)) % (type+'_start', markup[0],
+                                                    type+'_end', markup[1])
+                   for type, markup in self._MARKUP]
+        self._rules = re.compile('(?:' +'|'.join(regexps)+ ')', re.MULTILINE)
         self._parent = parent
 
     def _markup_handler(self, match):
-        type = [key for key, m in match.groupdict().items() if m][0]
+        type = [key for key, m in match.groupdict().items()
+                if m and not key in self._HELPER_PATTERNS][0]
         if match.group(type).startswith('!'):
             return match.group(type)[1:]
         start = False
@@ -84,26 +98,45 @@ class Formatter(object):
             del self._open[i:]
             to_close.reverse()
             # We have to close the nested open tags as-well.
-            return ''.join([self._formatter(t, close=True) for t in to_close])
+            return ''.join([self._formatter(t, match.groupdict(), close=True)
+                            for t in to_close])
         elif not end:
             if type in self._PAIR:
                 self._open.append(type)
-            return self._formatter(type)
+            return self._formatter(type, match.groupdict())
 
-    def _formatter(self, type, close=False):
+    def _formatter(self, type, groups, close=False):
         try:
-            return getattr(self, '_'+type+'_formatter')(close=close)
+            return getattr(self, '_'+type+'_formatter')(groups, close=close)
         except AttributeError:
             f = self._FORMAT[type]
             return type in self._PAIR and f[close and 1 or 0] or f
         
+    def _link_formatter(self, groups, close=False):
+        title = groups['title']
+        href = groups['href']
+        if not title:
+            if href.startswith('#'):
+                section = self._parent.find_section(href[1:])
+                title = section and section.title() or href[1:]
+            else:
+                title = href
+        return '<a href="%s">%s</a>' % (href, title)
+
+    def _uri_formatter(self, groups, close=False):
+        return self._link_formatter({'href': groups['uri'], 'title': None})
+
+    def _email_formatter(self, groups, close=False):
+        addr = groups['email']
+        return self._link_formatter({'href': 'mailto:'+addr, 'title': addr})
+
     def format(self, text):
         self._open = []
-        result = re.sub(self._RULES, self._markup_handler, text)
+        result = re.sub(self._rules, self._markup_handler, text)
         self._open.reverse()
         x = self._open[:]
         for type in x:
-            result += self._formatter(type, close=True)
+            result += self._formatter(type, {}, close=True)
         return result
         
     
@@ -116,8 +149,14 @@ class Parser(object):
     """
     _LIST_MARKER = "(?:\*|(?:[a-z]|\d+)(?:\)|\.))"
     
-    _SECTION_RE = re.compile(r"^(?P<level>=+) (?P<title>.*) (?P=level)\s*$",
+    _SECTION_RE = re.compile(r"^(?P<level>=+) (?P<title>.*) (?P=level)" + \
+                             r"(?:\s+(?:\*|(?P<anchor>[\w\d_-]+)))?\s*$",
                              re.MULTILINE)
+
+    _PRE_BLOCK_RE = re.compile(r"^(?:\{\{\{\s*$(.*?)^\}\}\}\s*$" + 
+                               r"|-----+\s*$(.*?)^-----+)\s*$",
+                               re.DOTALL|re.MULTILINE)
+
     _SPLITTER_RE = re.compile(r"\r?\n(?:(?:\s*\r?\n)+|(?=\s+" +
                               _LIST_MARKER +" ))")
 
@@ -125,16 +164,20 @@ class Parser(object):
                   "(?P<depth> +)(?P<type>"+ _LIST_MARKER +") (?P<content>.*)"),
                  ('table',
                   "\s*((\|[^\r\n\|]*)+\|\s*)+"),
+                 ('toc',
+                  "\s*(?:(?P<title>.+)\s+)?\@TOC\@\s*"),
                  )
     _REGEXPS = [r"^(?P<%s>%s)$" % (key, matcher) for key, matcher in _MATCHERS]
     _MATCHER = re.compile('(?:' + '|'.join(_REGEXPS) + ')', re.DOTALL)
-    _HELPER_PATTERNS = ('depth', 'content')
+    _HELPER_PATTERNS = ('depth', 'content', 'title')
     
     class _Section(object):
-        def __init__(self, title, level):
+        def __init__(self, title, anchor, level):
             assert isinstance(title, types.StringTypes)
+            assert isinstance(anchor, types.StringTypes) or anchor is None
             assert isinstance(level, types.IntType) and level >= 0
             self.title = title
+            self.anchor = anchor
             self.level = level
             self.parent = None
             self.content = None
@@ -151,7 +194,7 @@ class Parser(object):
         self._parent = parent
 
     def _parse_sections(self, text):
-        root = last = self._Section('__ROOT_SECTION__', 0)
+        root = last = self._Section('__ROOT_SECTION__', None, 0)
         while (1):
             m = self._SECTION_RE.search(text)
             if not m:
@@ -159,7 +202,8 @@ class Parser(object):
                 break
             last.content = self._parse_section_content(text[:m.start()])
             text = text[m.end():]
-            this = self._Section(m.group('title'), len(m.group('level')))
+            this = self._Section(m.group('title'), m.group('anchor'),
+                                 len(m.group('level')))
             if last.level < this.level:
                 parent = last
             elif last.level == this.level:
@@ -173,13 +217,27 @@ class Parser(object):
         return self._build_sections(root)
 
     def _build_sections(self, section):
-        subsections = [Section(self._parent, s.title, self._build_sections(s))
+        subsections = [Section(self._parent, s.title, self._build_sections(s),
+                               anchor=s.anchor)
                        for s in section.children]
         if section.content is None:
             return subsections
         else:
             return section.content + subsections
         
+    def _parse_section_content(self, text):
+        content = []
+        while (1):
+            m = self._PRE_BLOCK_RE.search(text)
+            if not m:
+                content += self._parse_blocks(text)
+                break
+            content += self._parse_blocks(text[:m.start()])
+            pre = m.group(1) or m.group(2)
+            content.append(PreformattedText(self._parent, pre))
+            text = text[m.end():]
+        return content
+
     def _identify_block(self, text):
         match = self._MATCHER.match(text)
         if match:
@@ -190,7 +248,7 @@ class Parser(object):
         else:
             return (text, 'paragraph', {})
 
-    def _parse_section_content(self, text):
+    def _parse_blocks(self, text):
         blocks = [self._identify_block(p)
                   for p in self._SPLITTER_RE.split(text) if len(p.strip()) > 0]
         content = []
@@ -219,6 +277,9 @@ class Parser(object):
         return Table(p, [TableRow(p, [TableCell(p, WikiText(p, x.strip()))
                                       for x in row.split('|')[1:-1]])
                          for row in block.strip().splitlines()])
+
+    def _make_toc(self, block, groups):
+        return TableOfContents(self._parent, title=groups['title'], depth=99)
     
     def _list_item_type(self, groups):
         t = groups.get('type')
