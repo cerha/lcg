8286493d6ab47f94f38073858aa958e9fcf766c6
cerha <cerha>
2005-04-07 14:33:22 +0000

    (ResourceNotFound): New exception class. (Resource._check_file): Raise `ResourceNotFound' when the resource was not found. (Resource._destination_file): Method removed. (Resource.export): Don't use `self._destination_file()'. Pass `infile' and `outfile' to `self._export()'. (Resource._export): New arguments `infile' and `outfile' replacing the old `dir'. (Media._FORCE_OUTPUT_FORMAT): Constant removed. (Media.__init__): Argument `tts_input' removed.  Don't care about `_FORCE_OUTPUT_FORMAT'. (Media._check_file): Method removed. (Media._find_source_file): Don't add the transcript dir to input dirs.  Omit the text formats from input formats. (Media._command, Media._open_stream_from_tts) (Media._open_stream_from_encoder): Methods removed. (Media._export): Don't care about TTS, just do the conversion if necessary. (Transcript._export): New arguments `infile' and `outfile' replacing the old `dir'.

--- lib/lcg/resources.py
+++ lib/lcg/resources.py
@@ -30,6 +30,12 @@ import codecs
 
 from lcg import *
 
+class ResourceNotFound(Exception):
+    def __init__(self, filename):
+        msg = "Resource file not found: %s" % filename
+        Exception.__init__(self, msg)
+    
+    
 class Resource(object):
     """Base resource class.
     
@@ -71,8 +77,8 @@ class Resource(object):
         self._check_file()
 
     def _check_file(self):
-        assert os.path.exists(self._src_path), \
-               "Resource file '%s' doesn't exist!" % self._src_path
+        if not os.path.exists(self._src_path):
+            raise ResourceNotFound(self._src_path)
         
     def _find_source_file(self, dirs, file):
         for d in dirs:
@@ -81,9 +87,6 @@ class Resource(object):
                 return path
         return os.path.join(dirs[0], file)
                 
-    def _destination_file(self, dir):
-        return os.path.join(dir, self._dst_path)
-
     def url(self):
         return '/'.join(self._dst_path.split(os.path.sep))
 
@@ -91,17 +94,22 @@ class Resource(object):
         return "%s_%s" % (self.__class__.__name__.lower(), id(self))
 
     def export(self, dir):
-        dst_path = self._destination_file(dir)
-        if (not os.path.exists(dst_path)
-            or (os.path.exists(self._src_path) and
-                os.path.getmtime(dst_path) < os.path.getmtime(self._src_path))
+        infile = self._src_path
+        outfile = os.path.join(dir, self._dst_path)
+        if (not os.path.exists(outfile)
+            or (os.path.exists(infile) and
+                os.path.getmtime(outfile) < os.path.getmtime(infile))
             or self._additional_export_condition()):
-            if not os.path.isdir(os.path.dirname(dst_path)):
-                os.makedirs(os.path.dirname(dst_path))
-            self._export(dir)
+            if not os.path.isdir(os.path.dirname(outfile)):
+                os.makedirs(os.path.dirname(outfile))
+            self._export(infile, outfile)
             
     def _additional_export_condition(self):
         return False
+
+    def _export(self, infile, outfile):
+        shutil.copy(infile, outfile)
+        print "%s: file copied." % self._destination_file(dir)
 
     def get(self):
         fh = open(self._src_path)
@@ -109,127 +117,49 @@ class Resource(object):
         fh.close()
         return data
             
-    def _export(self, dir):
-        shutil.copy(self._src_path, self._destination_file(dir))
-        print "%s: file copied." % self._destination_file(dir)
 
-
+        
 class Media(Resource):
     """Representation of a media object used within the content."""
     SUBDIR = 'media'
 
-    _FORCE_OUTPUT_FORMAT = 'mp3'
-    "If not None, all wave files will be converted to this format on output."
-    
-    def __init__(self, parent, file, shared=False, tts_input=None):
-        """Initialize the instance.
-
-        Arguments:
-
-          parent, file, shared -- See 'Resource.__init__()'.
-          tts_input -- if defined and the source file does not exist, the
-            destination file will be generated via TTS.  The given string will
-            be synthesized.
-
-        """
+    def __init__(self, parent, file, shared=False):
         basename, ext = os.path.splitext(file)
         assert ext in ('.ogg','.mp3','.wav'), "Unsupported media type: %s" % ext
-        if self._FORCE_OUTPUT_FORMAT is not None:
-            file = basename + '.' + self._FORCE_OUTPUT_FORMAT
-        self._tts_input = tts_input
         super(Media, self).__init__(parent, file, shared=shared)
-        
-    def _check_file(self):
-        if self._tts_input is None:
-            super(Media, self)._check_file()
         
     def _find_source_file(self, dirs, file):
         basename, extension = os.path.splitext(file)
-        dirs += (os.path.join(self._parent.src_dir(), Transcript.SUBDIR),)
-        for ext in ('.ogg','.mp3','.wav','.tts.txt', '.txt'):
+        for ext in ('.ogg','.mp3','.wav'):
             path = super(Media, self)._find_source_file(dirs, basename + ext)
             if os.path.exists(path):
                 return path
         return super(Media, self)._find_source_file(dirs, file)
 
-    def _command(self, which, errmsg):
-        var = 'LCG_%s_COMMAND' % which
+    def _export(self, infile, outfile):
+        input_format = os.path.splitext(infile)[1].upper()[1:]
+        output_format = os.path.splitext(outfile)[1].upper()[1:]
+        if input_format == output_format:
+            return super(Media, self)._export(infile, outfile)
+        elif input_format != 'WAV':
+            raise Exception("Unsupported conversion: %s -> %s" % \
+                            (input_format, output_format))
+        var = 'LCG_%s_COMMAND' % output_format
+        def cmd_err(msg):
+            raise Exception(msg % var + "\n" +
+                            "Specify a command encoding a wave file '%%infile' "
+                            "to an %s file '%%outfile'." % output_format)
         try:
-            return os.environ[var]
+            cmd = os.environ[var]
         except KeyError:
-            raise "Environment variable %s not set!\n" % var + errmsg
-
-    def _open_stream_from_tts(self):
-        if self._tts_input is not None:
-            text = self._tts_input
-        else:
-            fh = codecs.open(self._src_path,
-                             encoding=self._parent.input_encoding())
-            text = ''.join(fh.readlines())
-            fh.close()
-        cmd = self._command('TTS', "Specify a TTS command synthesizing " + \
-                            "the text on STDIN to a wave on STDOUT.")
-        print "  - generating with TTS: %s" % cmd
-        input, output = os.popen2(cmd, 'b')
-        input.write(text.encode(self._parent.input_encoding()))
-        input.close()
-        return output
-    
-    def _open_stream_from_encoder(self, output_format, wave):
-        # The tmp file is a hack.  It would be better to send the data into a
-        # pipe, but popen2 gets stuck while reading it.  Why?
-        tmp = os.tmpnam() + '.wav'
-        self._tmp_files.append(tmp)
-        f = open(tmp, 'wb')
-        copy_stream(wave, f)
-        f.close()
-        cmd = self._command(output_format, "Specify a command encoding a " + \
-                            "wave on STDIN to %s on STDOUT." % output_format)
-        print "  - converting to %s: %s" % (output_format, cmd)
-        output = os.popen('cat %s |' % tmp + cmd)
-        #input, output = os.popen2(convert_cmd)
-        #copy_stream(wave, input)
-        #input.close()
-        return output
-
-    
-    def _export(self, dir):
-        # Either create the file with tts or copy from source directory.
-        input_format = os.path.splitext(self._src_path)[1].upper()[1:]
-        output_format = os.path.splitext(self._dst_path)[1].upper()[1:]
-        if input_format == output_format and os.path.exists(self._src_path):
-            return super(Media, self)._export(dir)
-        dst_path = self._destination_file(dir)
-        wave = None
-        data = None
-        self._tmp_files = []
-        try:
-            print dst_path + ':'
-            # Open the input stream
-            if input_format == 'WAV' and os.path.exists(self._src_path):
-                wave = open(self._src_path)
-            elif input_format in ('TXT', 'TTS.TXT') or \
-                     self._tts_input is not None:
-                wave = self._open_stream_from_tts() 
-            else:
-                raise Exception("Unsupported conversion: %s -> %s" % \
-                                input_format, output_format)
-            if output_format == 'WAV':
-                data = wave
-            else:
-                data = self._open_stream_from_encoder(output_format, wave)
-            # Write the output stream
-            output_file = open(dst_path, 'wb')
-            copy_stream(data, output_file)
-            output_file.close()
-        finally:
-            if wave is not None:
-                wave.close()                
-            if data is not None:
-                data.close()
-            # This is just because of the hack in _open_output_stream().
-            for f in self._tmp_files:
-                os.remove(f)
+            cmd_err("Environment variable %s not set.")
+        if cmd.find("%infile") == -1 or cmd.find("%outfile") == -1:
+            cmd_err("Environment variable %s must refer to "
+                    "'%%infile' and '%%outfile'.")
+        print "%s: converting to %s: %s" % (outfile, output_format, cmd)
+        command = cmd.replace('%infile', infile).replace('%outfile', outfile)
+        if os.system(command):
+            raise IOError("Subprocess returned a non-zero exit status.")
         
         
 class Script(Resource):
@@ -267,23 +197,21 @@ class Transcript(Resource):
             
     def _additional_export_condition(self):
         return self._text is not None
-    
-    def _export(self, dir):
-        # Either create the file using the text or copy from source directory.
-        dst_path = self._destination_file(dir)
+
+    def _export(self, infile, outfile):
         if self._text is not None:
             text = self._text
         else:
-            fh = codecs.open(self._src_path,
-                             encoding=self._parent.input_encoding())
+            fh = codecs.open(infile, encoding=self._parent.input_encoding())
             try:
                 text = ''.join(fh.readlines())
             except UnicodeDecodeError, e:
-                raise Exception("Error while reading file %s: %s" %
-                                (self._src_path, e))
+                raise Exception("Error while reading file %s: %s" % (infile, e))
             fh.close()
         text = "\n\n".join([textwrap.fill(x)
                             for x in text.replace("\r\n", "\n").split("\n\n")])
-        output_file = open(dst_path, 'w')
-        output_file.write(text.encode('utf-8'))
-        output_file.close()
+        output = open(outfile, 'w')
+        try:
+            output.write(text.encode('utf-8'))
+        finally:
+            output.close()
