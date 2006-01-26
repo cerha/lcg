719d78102484da244810481e9505776c52514616
cerha <cerha>
2006-01-26 17:26:02 +0000

    *** empty log message ***

--- bin/lcgmake.py
+++ bin/lcgmake.py
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-"""Wiki documentation generator."""
+"""Simple and `quite' generic LCG generator."""
 
 import os
 import sys, getopt, os
