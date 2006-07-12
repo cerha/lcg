1a658ae5763536ec3b715ce565f278be4aae2301
cerha <cerha>
2006-07-12 13:36:39 +0000

    Import also from the new `html' module.

--- lib/lcg/export/__init__.py
+++ lib/lcg/export/__init__.py
@@ -17,10 +17,11 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 from export import *
+from html import *
 import ims
 import hhp
+from lcg import *
 
-# 
-for file in (export, ims, hhp):
+for file in (export, html, ims, hhp):
     file.__dict__.update(globals())
                                                                                                                                                       
