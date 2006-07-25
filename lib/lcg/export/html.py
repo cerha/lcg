e6f76aab18baeeec584f9fd122c021cf27ebff18
cerha <cerha>
2006-07-25 08:21:46 +0000

    (HtmlMarkupFormatter._link_formatter): New method /moved here from `MarkupFormatter'. (HtmlExporter._BODY_PARTS: New constant. (HtmlExporter.__init__): Pass `self' to the `HtmlMarkupFormatter' constructor. (HtmlExporter._output_file): New optional argument `lang'. (HtmlExporter._url): Method removed. (HtmlExporter._node_uri): New method. (HtmlExporter.uri): New method. (HtmlExporter._head): Added the `Content-Style-Type' meta tag. (HtmlExporter._styles, HtmlExporter._head) (HtmlExporter.page, HtmlStaticExporter._head): `url' -> `uri'. (HtmlExporter._body_parts): Iterate through `self._BODY_PARTS' and wrap them into a div automatically. (HtmlExporter._heading, HtmlExporter._language_selection) (HtmlExporter._content): New methods. (HtmlStaticExporter._BODY_PARTS): New constant. (HtmlStaticExporter._body_parts): Method removed. (HtmlStaticExporter._link): Method removed. (HtmlStaticExporter._rule): New method. (HtmlStaticExporter._navigation.link): New function. (HtmlStaticExporter._navigation): Use it.

--- lib/lcg/export/html.py
+++ lib/lcg/export/html.py
@@ -45,6 +45,36 @@ class HtmlMarkupFormatter(MarkupFormatter):
         else:
             return '</span>'
     
+    def _link_formatter(self, parent, title=None, href=None, anchor=None,
+                        resource_cls=None, close=False, **kwargs):
+        node = None
+        if resource_cls:
+            cls = globals()[resource_cls]
+            resource = parent.resource(cls, href, fallback=False)
+            if resource:
+                return '<a href="%s">%s</a>' % (resource.uri(), title or href)
+            else:
+                log("%s: Unknown resource: %s" % (parent.id(), href))
+        elif not href:
+            node = parent
+        elif href.find('@') == href.find('/') == -1:
+            node = parent.root().find_node(href)
+            if not node:
+                log("%s: Unknown node: %s" % (parent.id(), href))
+        target = node
+        if node and anchor:
+            target = node.find_section(anchor)
+            if target is None:
+                log("%s: Unknown section: %s:%s" %
+                    (parent.id(), node.id(), anchor))
+        if not target:
+            if anchor is not None:
+                href += '#'+anchor
+            target = Link.ExternalTarget(href, title or href)
+        l = Link(target, label=title)
+        l.set_parent(parent)
+        return l.export(self._exporter)
+    
     def _uri_formatter(self, parent, uri, close=False, **kwargs):
         return self._link_formatter(parent, href=uri, title=None)
 
@@ -54,12 +84,18 @@ class HtmlMarkupFormatter(MarkupFormatter):
     
 class HtmlExporter(Exporter):
     DOCTYPE = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">'
-
+    
+    _BODY_PARTS = ('heading',
+                   'language_selection',
+                   'content',
+                   )
+    
+    
     def __init__(self, stylesheet=None, inlinestyles=False):
         """Initialize the exporter for a given 'ContentNode' instance."""
         self._stylesheet = stylesheet
         self._inlinestyles = inlinestyles
-        self._formatter = HtmlMarkupFormatter()
+        self._formatter = HtmlMarkupFormatter(self)
         super(HtmlExporter, self).__init__()
     
     def _backref(self, node, target):
@@ -73,25 +109,33 @@ class HtmlExporter(Exporter):
         else:
             return None
     
-    def _output_file(self, node):
+    def _output_file(self, node, lang=None):
         """Return full pathname of node's output file relative to export dir."""
-        # TODO: This should be really done here, but some more code has to be
-        # rewritten.
-        return node.output_file()
+        assert isinstance(node, ContentNode)
+        name = node.id()
+        if lang is None:
+            lang = node.current_language_variant()
+        if lang is not None and len(node.language_variants()) > 1:
+            name += '.'+lang
+        return name + '.html'
 
-    def _url(self, target, relative=False):
-        """Return the URL as string."""
-        assert isinstance(target, (ContentNode, Section, Link.ExternalTarget))
+    def _node_uri(self, node, lang=None):
+        return self._output_file(node, lang=lang)
+
+    def uri(self, target, relative_to=None):
+        """Return the URI of the target as string."""
         if isinstance(target, ContentNode):
-            return self._output_file()
+            return self._node_uri(target)
         elif isinstance(target, Section):
-            if relative:
+            if relative_to is not None and relative_to is target.parent():
                 base = ''
             else:
-                base = self._url(target.parent())
+                base = self.uri(target.parent())
             return base + "#" + target.anchor()
+        elif isinstance(target, (Link.ExternalTarget, Resource)):
+            return target.uri()
         else:
-            return target.url()
+            raise Exception("Invalid URI target:", target)
     
     def _section_header(self):
         if self._backref_used[target]:
@@ -112,7 +156,7 @@ class HtmlExporter(Exporter):
                     for s in node.resources(Stylesheet)]
         else:
             return ['<link rel="stylesheet" type="text/css" href="%s">' % \
-                    s.url() for s in node.resources(Stylesheet)]
+                    s.uri() for s in node.resources(Stylesheet)]
             
     def _head(self, node):
         if self._stylesheet is not None:
@@ -124,25 +168,68 @@ class HtmlExporter(Exporter):
         tags = ['<title>%s</title>' % node.title()] + \
                ['<meta http-equiv="%s" content="%s">' % pair
                 for pair in (('Content-Type', 'text/html; charset=UTF-8'),
+                             ('Content-Language', node.language()),
                              ('Content-Script-Type', 'text/javascript'),
-                             ('Content-Language', node.language()))] + \
+                             ('Content-Style-Type', 'text/css'))] + \
                ['<meta name="%s" content="%s">' % pair for pair in meta] + \
                ['<script language="Javascript" type="text/javascript"' + \
-                ' src="%s"></script>' % s.url()
+                ' src="%s"></script>' % s.uri()
                 for s in node.resources(Script)]
         return '  '+'\n  '.join(tags + self._styles(node))
 
-    def _body_parts(self, node):
-        parts = ['<h1>%s</h1>' % node.title()]
-        if node is node.root() and node.language_variants():
-            import lcg
-            parts.append(lcg.LanguageSelection(node).export(self))
-        parts.append(_html.div(node.content().export(self), 'content'))
-        return parts
-    
     def _body(self, node):
-        return "\n".join(self._body_parts(node))
+        parts = [(getattr(self, '_'+part)(node), part.replace('_', '-'))
+                 for part in self._BODY_PARTS]
+        return "\n".join([_html.div(part, cls=cls)
+                          for part, cls in parts if part is not None])
 
+    def _heading(self, node):
+        return '<h1>%s</h1>' % node.title()
+
+    def _language_selection(self, node):
+        #handler = "location.href = this.form.language.options[this.form.language.selectedIndex].value"
+        #select = _html.form((_html.label(self._label, 'language-selection'),
+        #                     _html.select('language',
+        #                                  [(t.title(), t.url())
+        #                                   for t in self._targets],
+        #                                  id='language-selection',
+        #                                  selected=self._current.url()),
+        #                     _html.button(_('Switch'), handler)),
+        #                    cls='language-selection')
+        #radio = _html.fieldset([_html.radio('language', value=t.url(), id=tid,
+        #                                    onclick="location.href = this.value",
+        #                                    checked=(self._current.url()==t.url())
+        #                                    ) + \
+        #                        _html.label(flag.export(exporter) + t.title(), tid)
+        #                        for t, tid, flag in
+        #                        [(t, t.url().replace('.','-'), f)
+        #                         for t, f in zip(self._targets, self._flags)]],
+        #                       legend=self._label, cls='language-selection')
+        if node is not node.root() or len(node.language_variants()) <= 1:
+            return None
+        current = node.current_language_variant()
+        pairs = [(lang, language_name(lang))
+                 for lang in node.language_variants()]
+        pairs.sort(lambda a,b: cmp(a[0], b[0]))
+        self._targets = targets = []
+        self._flags = flags = []
+        for lang, name in pairs:
+            if lang == current:
+                name += ' ' + _('(*)') #_('(current)')
+            t = Link.ExternalTarget(self._node_uri(node, lang=lang), name)
+            if lang == current:
+                self._current = t
+            targets.append(t)
+            #flag = node.resource(Image, 'flags/%s.gif' % lang)
+            flags.append(None) #InlineImage(flag))
+        result = _('Choose your language:') + " " + \
+                 " |\n".join([Link(target).export(self) #+" "+ flag.export(self)
+                              for target, flag in zip(targets, flags)])
+        return result
+
+    def _content(self, node):
+        return node.content().export(self)
+    
     def format_wiki_text(self, parent, text):
         """Format text with wiki markup and return HTML."""
         if text:
@@ -151,7 +238,7 @@ class HtmlExporter(Exporter):
             return ''
     
     def page(self, node):
-        if 'audio.js' in [r.url().split('/')[-1]
+        if 'audio.js' in [r.uri().split('/')[-1]
                           for r in node.resources(Script)]:
             hack = ('\n<object id="media_player" height="0" width="0"'
 		    ' classid="CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6">'
@@ -193,38 +280,44 @@ class HtmlStaticExporter(HtmlExporter, FileExporter):
         }
 
     _INDEX_LABEL = None
+
+    _BODY_PARTS = ('navigation',
+                   'rule',
+                   'heading',
+                   'language_selection',
+                   'content',
+                   'rule',
+                   'navigation',
+                   )
     
     def _head(self, node):
         tags = ['<link rel="%s" href="%s" title="%s">' % \
-                (kind, n.url(), n.title())
+                (kind, self.uri(n), n.title())
                 for kind, n in (('start', node.root()), 
                                 ('prev', node.prev()),
                                 ('next', node.next()))
                 if n is not None and n is not node]
         return '\n  '.join([super(HtmlStaticExporter, self)._head(node)]+tags)
 
-    def _body_parts(self, node):
-        parts = super(HtmlStaticExporter, self)._body_parts(node)
-        if len(node.root().linear()) > 1:
-            parts += ('<hr class="navigation">', self._navigation(node))
-        return parts
-
-    def _link(self, node, label=None, key=None):
-        if node is None: return _("None")
-        if label is None: label = node.title(brief=True)
-        return _html.link(label, node.url(), title=node.title(),
-                          hotkey=not key or self._hotkey[key])
+    def _rule(self, node):
+        return '<hr class="hidden">'
     
     def _navigation(self, node):
-        nav = [_('Next') + ': ' + self._link(node.next(), key='next'),
-               _('Previous') + ': ' + self._link(node.prev(), key='prev')]
+        def link(node, label=None, key=None):
+            return node and _html.link(label or node.title(brief=True),
+                                       self.uri(node), title=node.title(),
+                                       hotkey=not key or self._hotkey[key]) \
+                                       or _("None")
+        if len(node.root().linear()) <= 1:
+            return None
+        nav = [_('Next') + ': ' + link(node.next(), key='next'),
+               _('Previous') + ': ' + link(node.prev(), key='prev')]
         hidden = ''
         if node is not node.root():
             p = node.parent()
             if p is not node.root():
-                nav.append(_("Up") + ': ' + self._link(p, key='up'))
+                nav.append(_("Up") + ': ' + link(p, key='up'))
             else:
-                hidden = "\n"+self._link(p, key='up', label='')
-            nav.append(self._link(node.root(), label=self._INDEX_LABEL,
-                                  key='index'))
-        return _html.div(' |\n'.join(nav) + hidden, 'navigation')
+                hidden = "\n" + link(p, key='up', label='')
+            nav.append(link(node.root(), label=self._INDEX_LABEL, key='index'))
+        return ' |\n'.join(nav) + hidden
