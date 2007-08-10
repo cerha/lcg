d51966f557849bb0b972d15f84442d02f8b05f9e
cerha <cerha>
2007-08-10 11:17:47 +0000

    (unindent_docstring): New method.

--- lib/lcg/util.py
+++ lib/lcg/util.py
@@ -147,6 +147,31 @@ def camel_case_to_lower(string, separator='-'):
     return separator.join([w.lower() for w in words])
 
 
+def unindent_docstring(docstring):
+    """Trim indentation and blank lines from docstring text and return it."""
+    if not docstring:
+        return docstring
+    lines = docstring.expandtabs().splitlines()
+    # Determine minimum indentation (first line doesn't count):
+    indent = sys.maxint
+    for line in lines[1:]:
+        stripped = line.lstrip()
+        if stripped:
+            indent = min(indent, len(line) - len(stripped))
+    # Remove indentation (first line is special):
+    trimmed = [lines[0].strip()]
+    if indent < sys.maxint:
+        for line in lines[1:]:
+            trimmed.append(line[indent:].rstrip())
+    # Strip off trailing and leading blank lines:
+    while trimmed and not trimmed[-1]:
+        trimmed.pop()
+    while trimmed and not trimmed[0]:
+        trimmed.pop(0)
+    # Return a single string:
+    return '\n'.join(trimmed)
+
+
 def positive_id(obj):
     """Return id(obj) as a non-negative integer."""
     result = id(obj)
