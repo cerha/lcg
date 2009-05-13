8041d72c9f1b313e5631226c116fd1ca8010f5e8
cerha <cerha>
2009-05-13 10:13:19 +0000

    Docstring improved.  Don't ignore *args in constructor.

--- doc/examples/lcgmake-basic-reader/src/index.py
+++ doc/examples/lcgmake-basic-reader/src/index.py
@@ -2,22 +2,29 @@
 import lcg
 
 class Reader(lcg.DocFileReader):
-    """This is a very simple reader which reads in one file and
-    processes it.  This reader doesn't do anything special. You could
-    however override its methods to make LCG behave the way you like.
+    """Simple LCG Reader example.
 
-    This example is useful as a template if you want to create your
-    own Reader.  If you want to just compile one text file, you should
-    simply use:
+    This is a very simple reader which reads in one file and processes it.  This
+    reader doesn't do anything special.  You could however override its methods
+    to customize the creation of the LCG document.
 
-            lcgmake file.txt
+    Since we want to read a structured text document, the reader is derived from
+    'lcg.DocFileReader'.  If we wanted a completely custom document creation
+    (reading source text from a database, constructing it on the fly from some
+    other source of data), we would derive just from 'lcg.Reader', but we would
+    need to implement more of its methods.
+
+    This reader is so simple, that it would be possible to achieve the same
+    functionality without even defining a reader.  LCG knows to read textual
+    documents by default (since 'lcg.DocFileReader' is the default reader
+    class).  Thus this example is useful only as a template for other custom
+    readers.  If you really wanted to just process one (structued) text file,
+    you could simply invoke 'lcgmake file.txt'.
+            
     """
-
-    def __init__(self, *args, **kwargs):
-        """Initialize the instance.        
-        """
-        super(Reader, self).__init__(id='src', **kwargs)
-
+    def __init__(self, id, *args, **kwargs):
+        # Force the name of the input file to the other file in the same directory.
+        super(Reader, self).__init__('src', *args, **kwargs)
 
     # Here you would override the DocFileReader methods
 
