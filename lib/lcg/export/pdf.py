745c00478e31d13b3381c640cac69e4a3638465c
Milan Zamazal <pdm@brailcom.org>
2010-08-17 19:07:17 +0200

    Fix of PDF footers vertical location

--- lib/lcg/export/pdf.py
+++ lib/lcg/export/pdf.py
@@ -110,7 +110,7 @@ class DocTemplate(reportlab.platypus.BaseDocTemplate):
                 if top:
                     y = self.height + self.bottomMargin - height
                 else:
-                    y = height - self.bottomMargin
+                    y = height
                 flowable.drawOn(canvas, x, y)
             header = first_page_header
             if page > 1 or header is None:
