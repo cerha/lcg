cerha/lcg

commit e65d546d84e7ea514046bb176e60d932e3f806b8

Fix image links for images with a thumbnail

lib/lcg/export/html.py
@@ -665,11 +665,11 @@ class HtmlExporter(Exporter):
         g = self._generator
         image = element.image()
         thumbnail = image.thumbnail()
+        link = None
         if thumbnail:
-            link = context.uri(image)
             image = thumbnail
-        else:
-            link = None
+            if True not in [isinstance(c, lcg.Link) for c in element.container_path()]:
+                link = context.uri(image)
         title = element.title()
         descr = element.descr()
         size = element.size()

lib/lcg/content.py
@@ -65,11 +65,16 @@ class Content(object):
         self._lang = lang
         super(Content, self).__init__()
         
-    def _container_path(self):
+    def container_path(self):
+        """Return a list of containers of the given element
+
+        The list is sorted from top to bottom, the last element being
+        this element itself."""
         path = [self]
         while path[0]._container is not None:
             path.insert(0, path[0]._container)
         return tuple(path)
+    _container_path = container_path # Backwards compatibility in use, but not overriding
 
     def sections(self, context):
         """Return the contained sections as a sequence of 'Section' instances.
@@ -918,7 +923,7 @@ class Section(Container):
         appears as the first and 'self' is always the last element of the list.
 
         """
-        return [c for c in self._container_path() if isinstance(c, Section)]
+        return [c for c in self.container_path() if isinstance(c, Section)]
     
     def section_number(self):
         """Return the number of this section within its container as int."""

commit 5041b7870f5eb2d02aaf2762bba1a523d0d56930

Fix thumbnail image link target
Fixes thumbnail link problem introduced by commit e65d546d84e7ea514046bb176e60d932e3f806b8.

lib/lcg/export/html.py
@@ -667,9 +667,9 @@ class HtmlExporter(Exporter):
         thumbnail = image.thumbnail()
         link = None
         if thumbnail:
-            image = thumbnail
             if True not in [isinstance(c, lcg.Link) for c in element.container_path()]:
                 link = context.uri(image)
+            image = thumbnail
         title = element.title()
         descr = element.descr()
         size = element.size()