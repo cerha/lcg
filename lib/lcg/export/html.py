8adb801bc0034c48476dc06cde2813310c7b8fed
Tomas Cerha <cerha@brailcom.org>
2012-10-01 12:31:56 +0200

    Rename CSS class of NewPage HTML export

--- lib/lcg/export/html.py
+++ lib/lcg/export/html.py
@@ -526,7 +526,7 @@ class HtmlExporter(Exporter):
         return self._generator.br()
 
     def _export_new_page(self, context, element):
-        return self._generator.hr(cls='lcg-new-page')
+        return self._generator.hr(cls='new-page')
 
     def _export_strong(self, context, element):
         return self._export_container(context, element, wrap=self._generator.strong)
