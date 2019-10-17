134cf5ea231da4347bf4e23295bd84ee41b19de4
Tomáš Cerha <cerha@oui.technology>
2019-10-17 09:19:18 +0200

    Allow custom plot axis label formatting
    Also includes implementation of formatters for common situations.  Date/time
    formatting is generalized and there is also a formatter for large monetary
    amounts using abbreviated representations such as 1.2 mil. or 3.45 bil. which
    avoids hard to read numbers and also exponential forms which matplotlib
    sometimes produces for big numbers.

--- lib/lcg/plot.py
+++ lib/lcg/plot.py
@@ -20,7 +20,11 @@ import datetime
 import io
 import lcg
 from matplotlib import pyplot
+import matplotlib.ticker
+import matplotlib.dates
 import pandas
+
+_ = lcg.TranslatableTextFactory('lcg')
 
 
 class LinePlot(lcg.InlineSVG):
@@ -38,7 +42,8 @@ class LinePlot(lcg.InlineSVG):
     """
 
     def __init__(self, data, size=(250, 120), title=None, xlabel=None, ylabel=None,
-                 plot_labels=None, annotate=False, grid=False):
+                 xformatter=None, yformatter=None, plot_labels=None, annotate=False,
+                 grid=False):
         """Arguments:
 
           data: 'pandas.DataFrame' instance or a sequence of sequences
@@ -53,6 +58,16 @@ class LinePlot(lcg.InlineSVG):
           title: Title to be displayed above the plot as a string.
           xlabel: x axis label as a human readable string.
           ylabel: y axis label as a human readable string.
+          xformatter: x axis tick label formater as a function of three
+            arguments (context, value, position), where context is the LCG
+            export context, value is the matplotlib's internal value
+            representation and position is the tick position numbered from 0.
+            This module defines several formatter classes for most common
+            cases, such as 'DateFormatter', 'DateTimeFormatter',
+            'MonetaryFormatter' which respect LCG localization.
+            'DateFormatter' is used automatically for the x axis if it contains
+            datetime values.
+          yformatter: as xformatter but for the y axis values.
           plot_labels: optional sequence of plot line labels as human readable
             strings.  Mostly useful for distinction when there are several plot
             lines.  The number of labels must correspond to the number of y
@@ -78,6 +93,8 @@ class LinePlot(lcg.InlineSVG):
         self._title = title
         self._xlabel = xlabel
         self._ylabel = ylabel
+        self._xformatter = xformatter
+        self._yformatter = yformatter
         self._plot_labels = plot_labels
         self._annotate = annotate
         if isinstance(grid, tuple):
@@ -93,13 +110,16 @@ class LinePlot(lcg.InlineSVG):
         fig, ax = pyplot.subplots(1, 1, figsize=size, sharex=True, sharey=True)
         for col, label in zip(df.columns, self._plot_labels or [None for x in df.columns]):
             ax.plot(df.index, getattr(df, col), label=label)
-        if isinstance(df.index[0], datetime.date):
-            # Set locale specific date format.
-            import matplotlib.dates
-            formatter = matplotlib.dates.DateFormatter(context.locale_data().date_format)
-            ax.xaxis.set_major_formatter(formatter)
+        xformatter, yformatter = self._xformatter, self._yformatter
+        if not xformatter and isinstance(df.index[0], datetime.date):
+            xformatter = DateFormatter()
             # Automatically rotate date labels if necessary to avoid overlapping.
             fig.autofmt_xdate()
+        for formatter, axis in ((xformatter, ax.xaxis), (yformatter, ax.yaxis)):
+            if formatter:
+                axis.set_major_formatter(matplotlib.ticker.FuncFormatter(
+                    lambda value, position, formatter=formatter: formatter(context, value, position)
+                ))
         if self._title:
             pyplot.title(self._title)
         if self._xlabel:
@@ -125,3 +145,148 @@ class LinePlot(lcg.InlineSVG):
         f = io.StringIO()
         fig.savefig(f, format='svg')
         return f.getvalue()
+
+
+class LocalizingFormatter(object):
+    """Common base class for LCG locale aware formatters."""
+
+    def _localizable(self, value):
+        raise NotImplementedError()
+
+    def __call__(self, context, value, position):
+        return context.localize(self._localizable(value))
+
+
+class DateTimeFormatter(LocalizingFormatter):
+    """Formats plot axis labels using LCG's locale aware datetime formatting.
+
+    Use instances of this class for the 'xformatter' and 'yformatter'
+    constructor arguments of 'LinePlot'.
+
+    """
+    def __init__(self, leading_zeros=False):
+        self._kwargs = dict(leading_zeros=leading_zeros)
+
+    def _localizable(self, value):
+        # Note, matplotlib returns a datetime with UTC timezone, but we
+        # want to avoid timezone conversion (or seeing the timezone on output
+        # when export context timezone is not set).
+        dt = matplotlib.dates.num2date(value).replace(tzinfo=None)
+        return lcg.LocalizableDateTime(dt, **self._kwargs)
+
+
+class DateFormatter(DateTimeFormatter):
+    """Formats plot axis labels using LCG's locale aware date formatting.
+
+    Use instances of this class for the 'xformatter' and 'yformatter'
+    constructor arguments of 'LinePlot'.
+
+    """
+    def __init__(self, leading_zeros=False):
+        self._kwargs = dict(leading_zeros=leading_zeros, show_time=False)
+
+
+class DecimalFormatter(LocalizingFormatter):
+    """Formats plot axis labels using LCG's locale aware numerical formatting.
+
+    Use instances of this class for the 'xformatter' and 'yformatter'
+    constructor arguments of 'LinePlot'.
+
+    """
+
+    _UNITS = tuple(reversed((
+        # Translators: Abbreviation for a million (10 ** 6).
+        (10 ** 6, ("mil.")),
+        # Translators: Abbreviation for a billion (10 ** 9).
+        (10 ** 9, ("bil.")),
+        # Translators: Abbreviation for a trillion (10 ** 12).
+        (10 ** 12, ("tril.")),
+    )))
+    _LOCALIZABLE = lcg.Decimal
+
+    def __init__(self, abbreviate=False, precision=None):
+        self._abbreviate = abbreviate
+        self._precision = precision
+
+    def _localizable(self, value):
+        unit = None
+        if self._precision is not None:
+            precision = self._precision
+        elif value % 1 == 0:
+            value = int(value)
+            if self._abbreviate:
+                for (amount, u) in self._UNITS:
+                    # Prefer 1,234,000 over 1.234 mil. and abbreviate only when there are
+                    # at most 2 significant digits, such as 1.23 mil.  This does not apply
+                    # for higher levels, so 1.234 bil. is better than 1,234 mil.
+                    precision = 2 if amount == 10 ** 6 else 3
+                    if value >= amount and value % (amount / 10 ** precision) == 0:
+                        value /= float(amount)
+                        unit = u
+                        while precision and int((10 ** precision) * value) % 10 == 0:
+                            precision -= 1
+                        break
+                else:
+                    precision = 0
+            else:
+                precision = 0
+        else:
+            precision = 2
+        result = self._LOCALIZABLE(value, precision=precision)
+        if unit:
+            result = lcg.concat(result, u'\xa0', unit)
+        return result
+
+
+class MonetaryFormatter(DecimalFormatter):
+    """Formats plot axis labels using LCG's locale aware monetary formatting.
+
+    Use instances of this class for the 'xformatter' and 'yformatter'
+    constructor arguments of 'LinePlot'.
+
+    """
+    _LOCALIZABLE = lcg.Monetary
+
+
+# TODO: This belongs to test.py in the python3 branch, but we temporarily
+# keep it here until python3 is merged to master to prevent merge conflicts.
+
+def _test_formatter(formatter, pairs):
+    exporter = lcg.HtmlExporter()
+    context = exporter.context(None, 'en')
+    for number, formatted in pairs:
+        assert formatter(context, number, 0).replace(u'\xa0', ' ') == formatted
+
+def test_decimal_formatter():
+    _test_formatter(lcg.plot.DecimalFormatter(), (
+        (1.0, '1'),
+        (3.4, '3.40'),
+        (120000, '120,000'),
+        (2300000, '2,300,000'),
+        (4234500000000, '4,234,500,000,000'),
+    ))
+
+def test_decimal_formatter_with_precision():
+    _test_formatter(lcg.plot.DecimalFormatter(precision=3), (
+        (1.0, '1.000'),
+        (3.4, '3.400'),
+        (1200, '1,200.000'),
+    ))
+
+def test_abbreviating_monetary_formatter():
+    _test_formatter(lcg.plot.MonetaryFormatter(abbreviate=True), (
+        (140000, '140,000'),
+        (2400000, '2.4 mil.'),
+        (3340000, '3.34 mil.'),
+        (4341000, '4,341,000'),
+        (5345100, '5,345,100'),
+        (6500000000, '6.5 bil.'),
+        (7530000000, '7.53 bil.'),
+        (8532000000, '8.532 bil.'),
+        (9000000000000, '9 tril.'),
+        (1100000000000, '1.1 tril.'),
+        (2120000000000, '2.12 tril.'),
+        (3123000000000, '3.123 tril.'),
+        (4123400000000, '4,123.400 bil.'),
+        (5 * 10 ** 12, '5 tril.'),
+    ))
