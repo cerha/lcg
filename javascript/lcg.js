cerha/lcg

commit 629b06c5943f6147488e1f149841b921dcc5ce29

Fix misc JavaScript errors from ES6/jquery conversion

javascript/lcg.js
@@ -1,7 +1,7 @@
 /* -*- coding: utf-8 -*-
  *
  * Copyright (C) 2012-2018 OUI Technology Ltd.
- * Copyright (C) 2019-2025 Tomáš Cerha <t.cerha@gmail.com>
+ * Copyright (C) 2019-2026 Tomáš Cerha <t.cerha@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -77,7 +77,7 @@ lcg.KeyHandler = class {
             // Returning false doesn't work here for some reason...
             event.preventDefault()
             event.stopPropagation()
-            command.bind(this)(event, $(event.target))
+            command.bind(this)(event, event.target)
         }
     }
 
@@ -213,10 +213,11 @@ lcg.Menu = class extends lcg.Widget {
      *
      */
 
+    _MANAGE_TABINDEX = true
+
     constructor(element) {
         super(element)
         this._init_menu(this.element.find('ul').first())
-        this._MANAGE_TABINDEX = true
     }
 
     _init_menu(ul) {
@@ -275,7 +276,7 @@ lcg.Menu = class extends lcg.Widget {
         let item
         if (this.items.length !== 0) {
             let current = this.element.find('a.current').first()
-            if (current) {
+            if (current.length) {
                 item = current
             } else {
                 item = this.items[0]
@@ -465,7 +466,7 @@ lcg.Notebook = class extends lcg.Menu {
         let i, callback, repeat
         let previously_selected_item = this._selected_item()
         super._select_item(item)
-        if (previously_selected_item !== item) {
+        if (!previously_selected_item || previously_selected_item[0] !== item[0]) {
             if (previously_selected_item) {
                 previously_selected_item.removeClass('current')
                 previously_selected_item[0]._lcg_notebook_page.hide()
@@ -588,11 +589,11 @@ lcg.FoldableTree = class extends lcg.Menu {
         super._init_item(item, prev, parent)
         item.attr('role', 'treeitem')
         let icon = item.find('.icon')
-        if (icon) {
+        if (icon.length) {
             icon.attr('role', 'presentation')
         }
         let label = item.find('.label')
-        if (label) {
+        if (label.length) {
             // Needed for VoiceOver to read the labels after adding the icon span
             // inside the a tag (because of MSIE as described in widgets.py...).
             let label_id = item.attr('id') + '-label'
@@ -602,7 +603,7 @@ lcg.FoldableTree = class extends lcg.Menu {
         let li = item.closest('li')
         // Append hierarchical submenu if found.
         let submenu = li.find('ul').first()
-        if (submenu) {
+        if (submenu.length) {
             if (li.hasClass('foldable')) {
                 if (!submenu.attr('id')) {
                     submenu.attr('id', item.attr('id') + '-submenu')
@@ -851,7 +852,7 @@ lcg.PopupMenuBase = class extends lcg.Menu {
                 },
             })
         } else {
-            menu.slideToggle(0.2, () => this._set_focus(selected_item))
+            menu.slideToggle(200, () => this._set_focus(selected_item))
         }
         this._on_touchstart_handler = (e) => { this._touch_moved = false }
         this._on_touchmove_handler = (e) => { this._touch_moved = true }
@@ -1139,7 +1140,7 @@ lcg.PopupMenuCtrl = class extends lcg.Widget {
         ctrl.on('keydown', this._on_key_down.bind(this))
         ctrl.find('.popup-arrow').on('click', e => menu.popup(e, ctrl))
         ctrl.attr('role', 'button')
-        ctrl.attr('aria-hapsopup', 'true')
+        ctrl.attr('aria-haspopup', 'true')
         ctrl.attr('aria-expanded', 'false')
         ctrl.attr('aria-controls', menu.element.attr('id'))
         if (selector) {
@@ -1243,7 +1244,7 @@ lcg.DropdownSelection = class extends lcg.PopupMenuBase {
     _select_item(item) {
         let previously_selected_item = this._selected_item()
         super._select_item(item)
-        if (previously_selected_item && previously_selected_item !== item) {
+        if (previously_selected_item && previously_selected_item[0] !== item[0]) {
             previously_selected_item.closest('li').removeClass('selected')
         }
         item.closest('li').addClass('selected')
@@ -1366,14 +1367,14 @@ lcg.CollapsibleWidget = class extends lcg.Widget {
         this.element.removeClass('collapsed')
         this.element.addClass('expanded')
         this._heading.attr('aria-expanded', 'true')
-        this._content.slideDown(0.2)
+        this._content.slideDown(200)
     }
 
     collapse() {
         this.element.removeClass('expanded')
         this.element.addClass('collapsed')
         this._heading.attr('aria-expanded', 'false')
-        this._content.slideUp(0.2)
+        this._content.slideUp(200)
     }
 
     toggle() {
@@ -1396,7 +1397,7 @@ lcg.CollapsibleSection = class extends lcg.CollapsibleWidget {
         let heading = this.element.find('h1,h2,h3,h4,h5,h6,h7,h8').first()
         heading.addClass('collapsible-section-heading')
         let backref = heading.find('a.backref')
-        if (backref) {
+        if (backref.length) {
             backref.attr('href', '')
         }
         return heading
@@ -1429,7 +1430,7 @@ lcg.CollapsiblePane = class extends lcg.CollapsibleWidget {
 lcg.AudioPlayer = class extends lcg.Widget {
 
     constructor(elements, swf_uri) {
-        super(element)
+        super(elements)
         this._volume = 0.8
         this._player = this.element.find('.jp-player')
         this._player.jPlayer({
@@ -1605,7 +1606,7 @@ lcg.AudioPlayer = class extends lcg.Widget {
             return false
         }
         let audio = this.element.find('audio')
-        if (audio) {
+        if (audio.length) {
             // If the browser supports the <audio> tag, jPlayer will use it and
             // we can get the media support information from its API.
             return !!(audio.canPlayType && audio.canPlayType(type.media).replace(/no/, ''))
@@ -1658,7 +1659,7 @@ lcg.Cookies = class {
     }
 
     set(name, value, days) {
-        let cookie = (name +'='+ escape(String(value)) + '; ' +
+        let cookie = (name +'='+ encodeURIComponent(String(value)) + '; ' +
                       'SameSite=Lax; ' +
                       'Path=' + escape(this.path))
         if (days) {
@@ -1685,7 +1686,7 @@ lcg.Cookies = class {
     }
 
     clearAll() {
-        for (let name of document.cookie.split(';').collect(s => s.split('=').first().trim())) {
+        for (let name of document.cookie.split(';').map(s => s.split('=')[0].trim())) {
             this.clear(name)
         }
     }

javascript/lcg-exercises.js
@@ -1,7 +1,7 @@
 /* -*- coding: utf-8 -*-
  *
  * Copyright (C) 2004-2017 OUI Technology Ltd.
- * Copyright (C) 2019, 2021 Tomáš Cerha <t.cerha@gmail.com>
+ * Copyright (C) 2019-2026 Tomáš Cerha <t.cerha@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -189,27 +189,6 @@ lcg.Exercise = class extends lcg.Widget {
         this._display_results()
     }
 
-    _slide_up(element, duration) {
-        if (Effect !== undefined) {
-            if (duration === 'undefined') {
-                duration = 0.2
-            }
-            new Effect.SlideUp(element, {duration: duration})
-        } else {
-            element.hide()
-        }
-    }
-
-    _slide_down(element, duration) {
-        if (Effect !== undefined) {
-            if (duration === undefined)
-                duration = 0.2
-            new Effect.SlideDown(element, {duration: duration})
-        } else {
-            element.show()
-        }
-    }
-
     _play_audio(uri) {
         if (!lcg.audio && typeof Audio !== 'undefined') {
             lcg.audio = new Audio()
@@ -333,7 +312,6 @@ lcg.FillInExercise = class extends lcg.Exercise {
 
     _init_field(field) {
         field.answer_index = this._last_answer_index++
-        console.log('>>', field.answer_index, field)
         $(field).on('keydown', this._on_key_down.bind(this))
         $(field).on('dblclick', this._on_eval_answer.bind(this))
         $(field).on('touchstart', this._on_touch_start.bind(this))
@@ -347,11 +325,11 @@ lcg.FillInExercise = class extends lcg.Exercise {
         if (element.disabled)
             element.disabled = false
         element.focus()
-        event.stop()
+        event.preventDefault()
+        event.stopPropagation()
     }
 
     _find_answer(field) {
-        console.log('--', field, field.answer_index, this._answers)
         const answers = this._answers[field.answer_index].split('|')
         const value = field.value
         let answer_index = 0
@@ -384,7 +362,6 @@ lcg.FillInExercise = class extends lcg.Exercise {
     }
 
     _error_handler(field) {
-        console.log('..', field)
         const found = this._find_answer(field)
         const index = found.index
         field.focus()
@@ -412,7 +389,7 @@ lcg.HiddenAnswers = class extends lcg.Exercise {
 
     constructor(form_name, answers, responses, messages) {
         super(form_name, answers, responses, messages)
-        this.element.find('.toggle-button').on('click', this._on_toggle_button)
+        this.element.find('.toggle-button').on('click', this._on_toggle_button.bind(this))
     }
 
     _recognize_field(field) {
@@ -420,7 +397,12 @@ lcg.HiddenAnswers = class extends lcg.Exercise {
     }
 
     _on_toggle_button(event) {
-        $(event.target).closest('.task').find('.answer').slideToggle(200)
+        let button = $(event.target).closest('.toggle-button')
+        let answer = button.closest('.task').find('.answer')
+        answer.slideToggle(200, () => {
+            let label = answer.is(':visible') ? this._messages["Hide Answer"] : this._messages["Show Answer"]
+            button.text(label)
+        })
         return false;
     }
 

commit 8295b1c5fbe81e1ac4bbe01f0e87561c4d4e7564

Pass jQuery objects to keyboard command handlers
The command handlers invoked from KeyHandler._on_key_down() work with jQuery
objects, just like the mouse event handlers, which pass them the same elements.
The keyboard handler, however, passed the plain DOM element of the event
target, so the handlers failed on it -- menu item data (stored through the
jQuery object) was not found and jQuery methods, such as hasClass(), were
undefined.  Keyboard operation of menus and foldable trees was thus broken,
as well as the keyboard commit in Pytis date picker.

Broken since 629b06c5, where it went unnoticed behind an unrelated JavaScript
error which aborted the page initialization anyway.

javascript/lcg.js
@@ -77,7 +77,9 @@ lcg.KeyHandler = class {
             // Returning false doesn't work here for some reason...
             event.preventDefault()
             event.stopPropagation()
-            command.bind(this)(event, event.target)
+            // The command handlers work with jQuery objects (as do the mouse
+            // event handlers, which pass them the same elements).
+            command.bind(this)(event, $(event.target))
         }
     }
 